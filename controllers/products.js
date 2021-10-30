d7c27f8207a9f3b42fe642fb308158d042be8c69
karim <karim@Azure.com>
2021-10-31 06:37:59 +1100

    add search filters to products api route

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,30 @@
-const getAllProductsStatic = async(req,res)=>{
-  res.status(200).json({msg:'products testing route'})
-}
+const { query } = require('express');
+const productModel = require('../models/product');
+const product = require('../models/product');
+const { search } = require('../routes/products');
 
-const getAllProducts = async(req,res)=>{
-  res.status(200).json({msg:'products route'})
-}
+//Postman testing route
+const getAllProductsStatic = async (req, res) => {
+	const products = await product.find({ featured: true, page: 2 });
+	res.status(200).json({ products, nbHits: products.length });
+};
 
-module.exports={
-  getAllProducts,
-  getAllProductsStatic
-}
+//products route
+const getAllProducts = async (req, res) => {
+	const { featured, company, name } = req.query;
+	const queryObject = {};
+
+	if (featured) queryObject.featured = featured === 'true' ? true : false;
+	if (company) queryObject.company = company;
+	if (name) queryObject.name = { $regex: name, $options: 'i' };
+
+	console.debug(queryObject);
+
+	const products = await productModel.find(queryObject);
+	res.status(200).json({ nbHits: products.length, products });
+};
+
+module.exports = {
+	getAllProducts,
+	getAllProductsStatic,
+};
--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router('');
 
+const {
+	getAllProducts,
+	getAllProductsStatic,
+} = require('../controllers/products');
 
-const {getAllProducts,getAllProductsStatic} = require ('../controllers/products');
+router.route('/').get(getAllProducts);
 
+//static route for testing purpose only
+router.route('/static').get(getAllProductsStatic);
 
-router.route('/').get(getAllProducts)
-//static route for testing purposes
-router.route('/static').get(getAllProductsStatic)
-
-module.exports = router
+module.exports = router;
